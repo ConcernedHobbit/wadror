1adf3221808ce6941383c3d696bdbbe4df6c7465
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-15 21:07:55 +0300

    Add signup as alias for user creation form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
 
   # Defines the root path route ("/")
   root 'breweries#index'
