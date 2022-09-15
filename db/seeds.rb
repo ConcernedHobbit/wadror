74c53d802fc20661b02fe81a93f15887675aff75
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-16 00:51:00 +0300

    Update seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,19 @@ b2 = Brewery.create name: "Malmgard", year: 2001
 b3 = Brewery.create name: "Weihenstephaner", year: 1040
 
 b1.beers.create name: "Iso 3", style: "Lager"
-b1.beers.create name: "Karhu", style: "Lager"
+karhu = b1.beers.create name: "Karhu", style: "Lager"
 b1.beers.create name: "Tuplahumala", style: "Lager"
 b2.beers.create name: "Huvila Pale Ale", style: "Pale Ale"
 b2.beers.create name: "X Porter", style: "Porter"
 b3.beers.create name: "Hefeweizen", style: "Weizen"
-b3.beers.create name: "Helles", style: "Lager"
+helles = b3.beers.create name: "Helles", style: "Lager"
+
+u1 = User.create username: "Olli"
+u2 = User.create username: "Vilma"
+
+karhu.ratings.create score: 70, user_id: u1.id
+karhu.ratings.create score: 35, user_id: u2.id
+helles.ratings.create score: 20, user_id: u2.id
+
+bc1 = BeerClub.create name: "Kumpulan kaljakerho", founded: 2018, city: "Helsinki"
+Membership.create beer_club_id: bc1.id, user_id: u1.id
