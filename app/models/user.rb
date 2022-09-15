94454bf25901d419ad49a9a0219084382feefea6
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-16 01:44:00 +0300

    Destroy user's dependents on user destruction

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
 
   validates :username, uniqueness: true,
