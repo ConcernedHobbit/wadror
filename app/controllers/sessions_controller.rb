54f9d466297729740013cefd40a7d713be3e5a07
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-15 21:38:18 +0300

    Improve sign in flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by username: params[:username]
-    session[:user_id] = user.id if user
-    redirect_to user
+    if user
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back #{user.username}!"
+    else
+      redirect_to signin_path, notice: "User #{params[:username]} does not exist!"
+    end
   end
 
   def destroy
