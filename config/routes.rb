2b0f7e3b2ecbb9b009675ffb8110f25c0cca7a68
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-15 20:19:19 +0300

    Use resource routing for rating

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
+  resources :ratings, only: [:index, :new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'ratings', to: 'ratings#index'
-  get 'ratings/new', to:'ratings#new'
-
-  post 'ratings', to: 'ratings#create'
 
   # Defines the root path route ("/")
   root 'breweries#index'
