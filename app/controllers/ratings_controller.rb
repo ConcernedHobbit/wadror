9f361d64e64e384554f784a6af87b3e13ff8a833
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-15 20:22:14 +0300

    Add delete button for ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -12,4 +12,10 @@ class RatingsController < ApplicationController
     Rating.create params.require(:rating).permit(:score, :beer_id)
     redirect_to ratings_path
   end
+
+  def destroy
+    rating = Rating.find(params[:id])
+    rating.delete
+    redirect_to ratings_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
-  resources :ratings, only: [:index, :new, :create]
+  resources :ratings, only: [:index, :new, :create, :destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
