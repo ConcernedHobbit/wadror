d6dfa47ddb954a64280ecabe9a720bdcc1afd00e
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-15 21:20:42 +0300

    Add signin, signout routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
   # Defines the root path route ("/")
   root 'breweries#index'
