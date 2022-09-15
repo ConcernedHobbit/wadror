3476f2a5eeae01083a211f14d35ed784cf65c62d
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-15 20:12:20 +0300

    Show beer and brewery names on rating form

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -5,6 +5,7 @@ class RatingsController < ApplicationController
 
   def new
     @rating = Rating.new
+    @beers = Beer.all
   end
 
   def create
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,4 +5,8 @@ class Beer < ApplicationRecord
   def average_rating
     ratings.sum(&:score).to_f / ratings.count
   end
+
+  def to_s
+    "#{self.name} (#{brewery.name})"
+  end
 end
