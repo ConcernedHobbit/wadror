d77906108c54efc95fd91ea9bc086a3642b16525
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-16 02:41:38 +0300

    Take factory bot into use

---
+++ spec/factories.rb
@@ -0,0 +1,24 @@
+FactoryBot.define do
+  factory :user do
+    username { "Pekka" }
+    password { "Foobar1" }
+    password_confirmation { "Foobar1" }
+  end
+
+  factory :brewery do
+    name { "anonymous" }
+    year { 1900 }
+  end
+
+  factory :beer do
+    name { "anonymous" }
+    style { "Lager" }
+    brewery
+  end
+
+  factory :rating do
+    score { 10 }
+    beer
+    user
+  end
+end
