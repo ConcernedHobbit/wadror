4a0ea5750459be58fa24feeb75fa4f6202adf221
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-16 02:50:29 +0300

    Fix seeding

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -5,6 +5,9 @@ class Membership < ApplicationRecord
   validate :unique_membership
 
   def unique_membership
+    return errors.add(:user, "should exist") unless user
+    return errors.add(:beer_club, "should exist") unless beer_club
+
     errors.add(:user, "already part of this club") if Membership.find_by(
       beer_club_id: beer_club.id,
       user_id: user.id
