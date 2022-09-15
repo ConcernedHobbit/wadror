b6445fd6889b0fe9a2352d546803acc1c31d52f8
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-15 19:32:05 +0300

    Add ratings skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'ratings', to: 'ratings#index'
 
   # Defines the root path route ("/")
   root 'breweries#index'
