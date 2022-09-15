6d7d6b947d3ffbd4a1f66e9466c75abc0a462d91
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-09-15 22:02:19 +0300

    Add many to many association for beer ratings

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   include RatingAverage
 
   has_many :ratings
+  has_many :beers, through: :ratings
 
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
