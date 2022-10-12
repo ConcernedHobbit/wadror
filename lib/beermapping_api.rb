552c2c0dfcccecf80663af94291d0c7a30092055
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-10-12 23:12:23 +0300

    use cache for beermapping api

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,5 +1,16 @@
 class BeermappingApi
   def self.places_in(city)
+    city = city.downcase
+
+    places = Rails.cache.read(city)
+    return places if places
+
+    places = get_places_in(city)
+    Rails.cache.write(city, places)
+    places
+  end
+
+  def self.get_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
