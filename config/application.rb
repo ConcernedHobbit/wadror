c5200a88f47361b2a807bdac671e956bc979babe
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-10-12 22:50:20 +0300

    move to api-based solution for beermapping
    skipped a bit of material to hide api key, felt dirty to commit

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Ratebeer
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
   end
 end
---
+++ lib/beermapping_api.rb
@@ -0,0 +1,22 @@
+class BeermappingApi
+  def self.places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    places = response.parsed_response["bmp_locations"]["location"]
+
+    return [] if places.is_a?(Hash) and places['id'].nil?
+
+    places = [places] if places.is_a?(Hash)
+    places.map do | place |
+      Place.new(place)
+    end
+  end
+
+  def self.key
+    return nil if Rails.env.test?
+    raise 'BEERMAPPING_APIKEY env variable not defined' if ENV['BEERMAPPING_APIKEY'].nil?
+
+    ENV.fetch('BEERMAPPING_APIKEY')
+  end
+end
