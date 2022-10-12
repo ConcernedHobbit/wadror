3c1d80feb1a0cbc1a2a1e6d81bf0f4a049f52770
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-10-12 18:20:08 +0300

    add basic beer creation system testing

---
+++ spec/features/beers_page_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "Beer" do
+  let!(:brewery) { FactoryBot.create :brewery, name: "Laitilan Wirvoitusjuomatehdas" }
+
+  it "when added with valid name, is added to the system" do
+    visit new_beer_path
+    fill_in("beer_name", with: "Kukko Lager")
+    select("Lager", from: "beer[style]")
+    select("Laitilan Wirvoitusjuomatehdas", from: "beer[brewery_id]")
+
+    expect {
+      click_button "Create Beer"
+    }.to change{Beer.count}.from(0).to(1)
+
+    expect(brewery.beers.count).to eq(1)
+  end
+
+  it "when added with invalid name, shows correct error message" do
+    visit new_beer_path
+    # Don't fill in name
+    select("Lager", from: "beer[style]")
+    select("Laitilan Wirvoitusjuomatehdas", from: "beer[brewery_id]")
+
+    click_button "Create Beer"
+
+    expect(page).to have_content "Name can't be blank"
+  end
+end
